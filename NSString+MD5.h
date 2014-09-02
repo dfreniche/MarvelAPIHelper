29205748d6ab17acb65193630fc8d5e5e7a1fbe9
Diego Freniche Brito <diego@freniche.com>
2014-09-02 07:47:10 +0200

    Added Category files

---
+++ NSString+MD5.h
@@ -0,0 +1,15 @@
+//
+//  NSString+MD5.h
+//  MarvelAPIDemo
+//
+//  Created by Diego Freniche Brito on 02/07/14.
+//  Copyright (c) 2014 Diego Freniche Brito. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (MD5)
+
+- (NSString *)MD5;
+
+@end
